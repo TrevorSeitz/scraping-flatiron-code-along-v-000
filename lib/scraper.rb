80d24d7c785b1a9f1153a165733c7b5d90c55fd2
Trevor Seitz <trevinla@gmail.com>
2018-03-27 16:13:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,21 @@ require 'open-uri'
 
 require_relative './course.rb'
 
-class Scraper
+class Scraper 
+
+  def get_courses
+    
+  end
+
+  def make_courses
+
+  end
+
+  def print_courses
+
+  end
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
