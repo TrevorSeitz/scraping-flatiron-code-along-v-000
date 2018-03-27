20a7adef21503c1b050c0cc97ba55ce870412044
Trevor Seitz <trevinla@gmail.com>
2018-03-27 17:39:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,12 @@ end
   end
 
   def make_courses
-
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 
   def print_courses
